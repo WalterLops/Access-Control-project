85e047b71c0ffb8772bacabb669e71bf62d53a2b
Walter Lopes <waltermagno02@gmail.com>
2023-04-15 15:44:05 -0300

    updates and fix

--- FilmesApi/Startup.cs
+++ FilmesApi/Startup.cs
@@ -34,6 +34,11 @@ namespace FilmesAPI
             services.AddDbContext<AppDbContext>(opts => opts.UseLazyLoadingProxies().UseMySQL(Configuration.GetConnectionString("CinemaConnection")));
             services.AddControllers();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            // services.AddScoped<CinemaService, CinemaService>();
+            // services.AddScoped<EnderecoService, EnderecoService>();
+            // services.AddScoped<FilmeService, FilmeService>();
+            // services.AddScoped<GerenteService, GerenteService>();
+            // services.AddScoped<SessaoService, SessaoService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
